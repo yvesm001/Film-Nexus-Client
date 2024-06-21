85fa24c3e35cebc5c3009a45e84ad56d8c0c9d49
Yves Martinez <yves.oliver.m@gmail.com>
2024-06-21 12:54:20 -0400

    login working

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { useState, useContext } from "react";
+import { AuthContext } from "../context/auth.context";
 
-export default function LoginForm() {
+function LoginForm() {
+  const [loginInfo, setLoginInfo] = useState({
+    loginInfo: "",
+    password: "",
+  });
+
+  const { login } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    setLoginInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        login(loginInfo);
+      }}
+    >
+      <label htmlFor="loginInfo">username / email</label>
+      <input
+        type="text"
+        name="loginInfo"
+        value={loginInfo.loginInfo}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="password">password</label>
+      <input
+        type="password"
+        name="password"
+        value={loginInfo.password}
+        onChange={handleChange}
+      />
+      <button type="submit">login</button>
+    </form>
+  );
 }
+
+export default LoginForm;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import LoginForm from '../components/LoginForm'
 
 export default function LoginPage() {
   return (
     <div>
-      <p>Login</p>
+    <h3>Log in</h3>
+      <LoginForm />
     </div>
   )
 }
