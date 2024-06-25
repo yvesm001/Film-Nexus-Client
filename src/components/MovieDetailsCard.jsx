37d42d9a541041939919561bd5acf39e74f41edf
Yves Martinez <yves.oliver.m@gmail.com>
2024-06-25 17:24:54 -0400

    favorites and watchlist working

--- src/components/MovieDetailsCard.jsx
+++ src/components/MovieDetailsCard.jsx
@@ -6,55 +6,93 @@ import EditForm from "./EditForm";
 
 export default function MovieDetailsCard({ movie }) {
   const [toggleEdit, setToggleEdit] = useState(false);
-  const { deleteMovie, addToWatchlist, removeFromWatchlist, watchlist, getWatchlist } = useContext(MovieContext);
+  const {
+    deleteMovie,
+    addToWatchlist,
+    removeFromWatchlist,
+    watchlist,
+    getWatchlist,
+    addToFavorites,
+    removeFromFavorites,
+    favorites,
+    getFavorites,
+  } = useContext(MovieContext);
   const { user } = useContext(AuthContext);
   const [inWatchlist, setInWatchlist] = useState(false);
+  const [inFavorites, setInFavorites] = useState(false);
 
   useEffect(() => {
-    // Fetch the watchlist if not already done
     if (!watchlist) {
       getWatchlist();
     }
-    // Check if the movie is in the watchlist
-    if (watchlist && watchlist.some(item => item._id === movie._id)) {
+    if (!favorites) {
+      getFavorites();
+    }
+
+    if (watchlist && watchlist.some((item) => item._id === movie._id)) {
       setInWatchlist(true);
     } else {
       setInWatchlist(false);
     }
-  }, [watchlist, movie._id, getWatchlist]);
+
+    if (favorites && favorites.some((movie) => movie._id === movie._id)) {
+      setInFavorites(true);
+    } else {
+      setInFavorites(false);
+    }
+  }, [watchlist, favorites, movie._id, getWatchlist, getFavorites]);
 
   const handleWatchlistAction = async () => {
     try {
-        if (inWatchlist) {
-            await removeFromWatchlist(movie._id);
-            setInWatchlist(false);
-        } else {
-            await addToWatchlist(movie._id);
-            setInWatchlist(true)
-        }
+      if (inWatchlist) {
+        await removeFromWatchlist(movie._id);
+        setInWatchlist(false);
+      } else {
+        await addToWatchlist(movie._id);
+        setInWatchlist(true);
+      }
     } catch (error) {
-        console.log("Error updating watchlist", error)
+      console.log("Error updating watchlist", error);
     }
-  }
+  };
 
-//   const handleAddToWatchlist = async () => {
-//     console.log("Add to watchlist clicked");
-//     try {
-//       await addToWatchlist(movie._id);
-//       console.log("Movie added to watchlist");
-//     } catch (error) {
-//       console.log("Error adding movie to watchlist", error);
-//     }
-//   };
+  const handleFavoritesAction = async () => {
+    try {
+      if (inFavorites) {
+        await removeFromFavorites(movie._id);
+        setInFavorites(false);
+      } else {
+        await addToFavorites(movie._id);
+        setInFavorites(true);
+      }
+    } catch (error) {
+      console.log("Error updating favorites", error);
+    }
+  };
+
+  //   const handleAddToWatchlist = async () => {
+  //     console.log("Add to watchlist clicked");
+  //     try {
+  //       await addToWatchlist(movie._id);
+  //       console.log("Movie added to watchlist");
+  //     } catch (error) {
+  //       console.log("Error adding movie to watchlist", error);
+  //     }
+  //   };
   return (
     <div>
       <h1>{movie.title}</h1>
       <h2>{movie.releaseYear}</h2>
       <h3>{movie.description}</h3>
       {user && (
-        <button onClick={handleWatchlistAction}>
-          {inWatchlist ? "Remove from Watchlist" : "Add to Watchlist"}
-        </button>
+        <>
+          <button onClick={handleWatchlistAction}>
+            {inWatchlist ? "Remove from Watchlist" : "Add to Watchlist"}
+          </button>
+          <button onClick={handleFavoritesAction}>
+            {inFavorites ? "Remove from favorites" : "Add to Favorites"}
+          </button>
+        </>
       )}
       {user && user.isAdmin && (
         <>
--- src/pages/WatchlistPage.jsx
+++ src/pages/WatchlistPage.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useContext } from "react";
 import { MovieContext } from "../context/movie.context";
 import MovieCard from "../components/MovieCard";
+import { AuthContext } from "../context/auth.context";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function WatchlistPage() {
   const { watchlist, getWatchlist } = useContext(MovieContext);
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchWatchlist = async () => {
@@ -15,6 +20,16 @@ export default function WatchlistPage() {
     };
     fetchWatchlist();
   }, []);
+
+  //MESSAGE USER SEES IF THEY ARE NOT LOGGED IN
+  if (!user) {
+    return (
+      <p>
+        <Link to="/signup">Sign up</Link> or <Link to="/login">login</Link> to
+        add movies to your watchlist
+      </p>
+    );
+  }
 
   return (
     <div>
