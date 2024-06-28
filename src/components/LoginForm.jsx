3dd349436199721af3e21809fc13a47a103adc63
Yves Martinez <yves.oliver.m@gmail.com>
2024-06-28 01:37:58 -0400

    fixed signup/login forms to match

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -25,21 +25,16 @@ function LoginForm() {
         <div className="container py-5 h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-              <div
-                className="card"
-                style={{ backgroundColor: "#c6c6c6", borderRadius: "1rem" }}
-              >
+              <div className="review-form">
                 <div className="card-body p-5 text-center">
                   <div className="mb-md-5 mt-md-4 pb-5">
                     <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-                    <p className=" mb-5">
-                      Please enter your login and password!
-                    </p>
+                    <p className="mb-5">Please enter your email and password</p>
 
-                    <div
-                      data-mdb-input-init
-                      className="form-outline form-white mb-4"
-                    >
+                    <div className="form-outline form-white mb-4">
+                      <label className="form-label" htmlFor="typeEmailX">
+                        Email
+                      </label>
                       <input
                         type="email"
                         id="typeEmailX"
@@ -48,15 +43,12 @@ function LoginForm() {
                         value={loginInfo.loginInfo}
                         onChange={handleChange}
                       />
-                      <label className="form-label" htmlFor="typeEmailX">
-                        Email
-                      </label>
                     </div>
 
-                    <div
-                      data-mdb-input-init
-                      className="form-outline form-white mb-4"
-                    >
+                    <div className="form-outline form-white mb-4">
+                      <label className="form-label" htmlFor="typePasswordX">
+                        Password
+                      </label>
                       <input
                         type="password"
                         id="typePasswordX"
@@ -65,28 +57,17 @@ function LoginForm() {
                         value={loginInfo.password}
                         onChange={handleChange}
                       />
-                      <label className="form-label" htmlFor="typePasswordX">
-                        Password
-                      </label>
                     </div>
 
-                    <button
-                      data-mdb-button-init
-                      data-mdb-ripple-init
-                      className="btn btn-success btn-lg px-5"
-                      type="submit"
-                    >
-                      Login
-                    </button>
+                    <button type="submit">Login</button>
                   </div>
 
                   <div>
                     <p className="mb-0">
                       Don't have an account?{" "}
-                      <Link to="/signup" className=" fw-bold text-body">
+                      <Link to="/signup" className="fw-bold">
                         Sign Up
                       </Link>
-
                     </p>
                   </div>
                 </div>
--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -40,102 +40,85 @@ export default function SignupForm() {
 
   return (
     <section className="vh-100 bg-image">
-      <div className="mask d-flex align-items-center h-100 gradient-custom-3">
-        <div className="container h-100">
-          <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-12 col-md-9 col-lg-7 col-xl-6">
-              <div className="card" style={{ borderRadius: "15px" }}>
-                <div
-                  className="card-body p-5"
-                  style={{ backgroundColor: "#c6c6c6", borderRadius: "15px" }}
-                >
-                  <h2 className="text-uppercase text-center mb-5">
-                    Create an account
-                  </h2>
+  <div className="mask d-flex align-items-center h-100 gradient-custom-3">
+    <div className="container h-100">
+      <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div className="review-form">
+            <div className="card-body p-5">
+              <h2 className="text-uppercase text-center mb-5">Create an account</h2>
 
-                  <form onSubmit={handleSubmit}>
-                    <div data-mdb-input-init className="form-outline mb-4">
-                      <input
-                        type="text"
-                        id="form3Example1cg"
-                        className="form-control form-control-lg"
-                        name="username"
-                        value={signupInfo.username}
-                        onChange={handleChange}
-                      />
-                      <label className="form-label" htmlFor="form3Example1cg">
-                        Username
-                      </label>
-                    </div>
-
-                    <div data-mdb-input-init className="form-outline mb-4">
-                      <input
-                        type="email"
-                        id="form3Example3cg"
-                        className="form-control form-control-lg"
-                        name="email"
-                        value={signupInfo.email}
-                        onChange={handleChange}
-                      />
-                      <label className="form-label" htmlFor="form3Example3cg">
-                        Your Email
-                      </label>
-                    </div>
-
-                    <div data-mdb-input-init className="form-outline mb-4">
-                      <input
-                        type="password"
-                        id="form3Example4cg"
-                        className="form-control form-control-lg"
-                        name="password"
-                        value={signupInfo.password}
-                        onChange={handleChange}
-                      />
-                      <label className="form-label" htmlFor="form3Example4cg">
-                        Password
-                      </label>
-                    </div>
-
-                    <div data-mdb-input-init className="form-outline mb-4">
-                      <input
-                        type="password"
-                        id="form3Example4cdg"
-                        className="form-control form-control-lg"
-                        name="repeatPassword"
-                        value={signupInfo.repeatPassword}
-                        onChange={handleChange}
-                      />
-                      <label className="form-label" for="form3Example4cdg">
-                        Repeat your password
-                      </label>
-                    </div>
-
-                    {!passwordsMatch && <p>Passwords do not match</p>}
-
-                    <div className="d-flex justify-content-center">
-                      <button
-                        type="submit"
-                        data-mdb-button-init
-                        data-mdb-ripple-init
-                        className="btn btn-success btn-block btn-lg text-white"
-                      >
-                        Register
-                      </button>
-                    </div>
-
-                    <p className="text-center text-muted mt-5 mb-0">
-                      Have already an account?{" "}
-                      <Link to="/login" className="fw-bold text-body">
-                        <u>Login here</u>
-                      </Link>
-                    </p>
-                  </form>
+              <form onSubmit={handleSubmit}>
+                <div className="form-outline mb-4">
+                  <label className="form-label" htmlFor="form3Example1cg">Username</label>
+                  <input
+                    type="text"
+                    id="form3Example1cg"
+                    className="form-control form-control-lg"
+                    name="username"
+                    value={signupInfo.username}
+                    onChange={handleChange}
+                  />
                 </div>
-              </div>
+
+                <div className="form-outline mb-4">
+                  <label className="form-label" htmlFor="form3Example3cg">Your Email</label>
+                  <input
+                    type="email"
+                    id="form3Example3cg"
+                    className="form-control form-control-lg"
+                    name="email"
+                    value={signupInfo.email}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="form-outline mb-4">
+                  <label className="form-label" htmlFor="form3Example4cg">Password</label>
+                  <input
+                    type="password"
+                    id="form3Example4cg"
+                    className="form-control form-control-lg"
+                    name="password"
+                    value={signupInfo.password}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="form-outline mb-4">
+                  <label className="form-label" htmlFor="form3Example4cdg">Repeat your password</label>
+                  <input
+                    type="password"
+                    id="form3Example4cdg"
+                    className="form-control form-control-lg"
+                    name="repeatPassword"
+                    value={signupInfo.repeatPassword}
+                    onChange={handleChange}
+                  />
+                </div>
+
+                {!passwordsMatch && <p>Passwords do not match</p>}
+
+                <div className="d-flex justify-content-center">
+                  <button type="submit">
+                    Register
+                  </button>
+                </div>
+
+                <p className="text-center mt-5 mb-0">
+                  Already have an account?{" "}
+                  <Link to="/login" className="fw-bold ">
+                    <u>Login here</u>
+                  </Link>
+                </p>
+              </form>
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </div>
+</section>
+
   );
 }
